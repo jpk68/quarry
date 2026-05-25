ae3d2412f0c6969df3190d3e45b25618f6c1f60a
jpk68 <jpk68@tutanota.com>
2026-05-25 15:56:27 -0400

    net: improve peer list handling

--- src/net/server.zig
+++ src/net/server.zig
@@ -16,28 +16,18 @@ const BanInfo = struct {
     expiration_timestamp: u64,
 };
 
-fn handlePeer(self: *Server, stream: net.Stream) void {
-    log.info("Accepted connection from {}", .{stream.socket.address});
-
-    const entry = PeerInfo{
-        .host = stream.socket.address,
-        .failed_connection_count = 0,
-        .last_seen_timestamp = 0,
-    };
-
-    self.peer_list.append(self.allocator, entry) catch {};
-
-    log.debug("Current peer list: {}", .{self.peer_list});
-}
-
 pub const Server = struct {
     allocator: Allocator,
     io: Io,
 
     peer_id: u64,
     num_connections: usize,
+
     peer_list: std.ArrayList(PeerInfo) = .empty,
+    peers_lock: Io.Mutex = .init,
+
     ban_list: std.ArrayList(BanInfo) = .empty,
+    bans_lock: Io.Mutex = .init,
 
     pub fn init(allocator: Allocator, io: Io) !*Server {
         const self = try allocator.create(Server);
@@ -55,35 +45,87 @@ pub const Server = struct {
         self.allocator.destroy(self);
     }
 
+    fn isBanned(self: *Server, addr: net.IpAddress) bool {
+        self.bans_lock.lockUncancelable(self.io);
+        defer self.bans_lock.unlock(self.io);
+
+        const now = std.time.timestamp();
+
+        for (self.ban_list.items) |b| {
+            if (std.meta.eql(b.host, addr) and @as(u64, @intCast(now)) < b.expiration_timestamp) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    fn removePeer(self: *Server, addr: net.IpAddress) void {
+        self.peers_lock.lockUncancelable(self.io);
+        defer self.peers_lock.unlock(self.io);
+
+        const len = self.peer_list.items.len;
+
+        for (0..len) |from_end| {
+            const i = len - from_end - 1;
+
+            if (std.meta.eql(self.peer_list.items[i].host, addr)) {
+                _ = self.peer_list.swapRemove(i);
+            }
+        }
+
+        if (self.num_connections > 0) {
+            self.num_connections -= 1;
+        }
+    }
+
+    fn handlePeer(self: *Server, stream: net.Stream) !void {
+        defer stream.close(self.io);
+        defer self.removePeer(stream.socket.address);
+
+        log.info("Accepted peer {}", .{stream.socket.address});
+
+        const entry = PeerInfo{
+            .host = stream.socket.address,
+            .failed_connection_count = 0,
+            .last_seen_timestamp = 0,
+        };
+
+        self.peer_list.append(self.allocator, entry) catch {};
+
+        log.debug("Current peer list: {}", .{self.peer_list});
+    }
+
     pub fn start(self: *Server) !void {
         // Initialize PRNG and obtain its interface. Uses Xoshiro256 by default.
-        const rand_inst: std.Random.IoSource = .{ .io = self.io };
-        const rand: std.Random = rand_inst.interface();
+        const rand_io: std.Random.IoSource = .{ .io = self.io };
+        const rand: std.Random = rand_io.interface();
 
         // Set peer_id to a random u64
         self.peer_id = rand.int(u64);
 
-        log.debug("Set random peer ID: {x}", .{self.peer_id});
+        log.debug("Set random local peer ID: {x}", .{self.peer_id});
 
         // TODO determine based on sidechain type
         const port: u16 = 7777;
-        const host = try net.IpAddress.parse("127.0.0.1", port);
+        const addr = try net.IpAddress.parse("127.0.0.1", port);
 
-        var tcp = try net.IpAddress.listen(&host, self.io, .{});
+        var tcp = try net.IpAddress.listen(&addr, self.io, .{});
         defer tcp.deinit(self.io);
 
-        log.info("Listening on port {}", .{port});
+        log.info("Listening on {}", .{addr});
 
         var group: Io.Group = .init;
         defer group.cancel(self.io);
 
         while (true) {
             const stream = try tcp.accept(self.io);
-            errdefer stream.close(self.io);
 
-            log.info("Accepted connection", .{});
+            _ = group.concurrent(self.io, handlePeer, .{ self, stream }) catch |err| {
+                log.err("Failed to create peer handler: {}", .{err});
 
-            try group.concurrent(self.io, handlePeer, .{ self, stream });
+                stream.close(self.io);
+            };
         }
     }
 };
